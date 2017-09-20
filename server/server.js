c04f19e0cd9f35b196149dfc7d76d5c5fcc150d2
Ravindra <reachravindrap@gmail.com>
2017-09-20 12:52:22 +0530

    init commit 2

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const path = require('path');
+const http = require('http');
 const express =  require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port =process.env.PORT||3000;
 var app = express();
+var server = http.createServer(app);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+server.listen(port, () => {
     console.log('App listening on port 3000!');
 });
